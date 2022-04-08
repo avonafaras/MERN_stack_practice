7b993021f1c700812f949983512cd73cb6314214
Anna <annasarafanova91@gmail.com>
2022-04-08 10:34:27 -0700

    Basic routes

--- server.js
+++ server.js
@@ -1,11 +1,16 @@
 const express = require('express');
+const connectDB = require('./config/db')
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+connectDB();
+
 app.get('/', (req, res) => {
   res.send('hello')
 })
+
+app.use('/api/users', require('./routes/api/users'))
 
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`))
